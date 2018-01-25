439dcf6f3d75d4e291cc488e75ac641278a13667
Lecus <16281057@qq.com>
2018-01-25 10:32:39 +0800

    map部分利用reduce求list积

---
+++ multiply_map.py
@@ -0,0 +1,11 @@
+#编写一个prod()函数，可以接受一个list并利用reduce()求积：
+from functools import reduce
+def prod(L):
+    def multiply(x, y):
+        return x * y
+    return reduce(multiply, L)
+print('3 * 5 * 7 * 9 =', prod([3, 5, 7, 9]))
+if prod([3, 5, 7, 9]) == 945:
+    print('测试成功!')
+else:
+    print('测试失败!')
